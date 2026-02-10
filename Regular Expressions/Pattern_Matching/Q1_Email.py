2d92861f9d50bbbbc65cb1106bfc464493ff6ff3
Yukti123456 <90753314+Yukti123456@users.noreply.github.com>
2026-02-10 19:56:26 +0530

    Create Q1_Email.py

---
+++ Regular Expressions/Pattern_Matching/Q1_Email.py
@@ -0,0 +1,19 @@
+Q1- Validate email addresses: Must contain @ Symbol, Domain, and extension.
+
+
+import re
+emails = [
+    "test@gmail.com",
+    "abc@company.org",
+    "user@domain.in",
+    "wrongemail.com",
+    "user@domain"
+]
+
+pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+for email in emails:
+    if re.match(pattern, email):
+        print(email, ": Valid")
+    else:
+        print(email, ":  Invalid")
