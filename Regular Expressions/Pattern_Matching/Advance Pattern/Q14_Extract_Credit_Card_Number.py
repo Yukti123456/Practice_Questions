e3e0ebafcedf7d18d5df1c8ed3f088e450f88557
Yukti123456 <90753314+Yukti123456@users.noreply.github.com>
2026-02-13 14:35:59 +0530

    Add script to extract credit card numbers from text
    This script extracts credit card numbers from a given text, identifying groups of 4 digits separated by spaces or hyphens.

---
+++ Regular Expressions/Pattern_Matching/Advance Pattern/Q14_Extract_Credit_Card_Number.py
@@ -0,0 +1,17 @@
+#Q.14_Extract credit card numbers(groups of 4 digits separated by spaces or hypens).
+
+import re
+
+Text ="""Riya used her card 1234 5678 9012 3456 for shopping.
+      Her brother mistakenly entered 9876-5432-1098-7654 on another site.
+
+      Invalid entries found were:
+      1234567890123456,
+      1234 567 8901 2345,
+      1234-5678-9012."""
+      
+Pattern = r'\b\d{4}[\s-]\d{4}[\s-]\d{4}[\s-]\d{4}\b'
+
+Matches= re.findall(Pattern,Text)
+
+print("Credit Card Numbers: ",Matches)
