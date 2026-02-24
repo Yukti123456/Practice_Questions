c2ed431839400219d7155fc1853d62abdcfaca6f
Yukti123456 <90753314+Yukti123456@users.noreply.github.com>
2026-02-24 11:36:27 +0530

    Update Q12.sql

--- SQL/Aggregation/Q12.sql
+++ SQL/Aggregation/Q12.sql
@@ -1,8 +1,7 @@
-SELECT e1.*
-FROM employees e1
-WHERE (
-    SELECT COUNT(DISTINCT e2.salary)
-    FROM employees e2
-    WHERE e2.dept_id = e1.dept_id
-    AND e2.salary > e1.salary
-) < 3;
+SELECT emp_name
+FROM employees e
+WHERE salary > (
+    SELECT AVG(salary)
+    FROM employees
+    WHERE dept_id = e.dept_id
+);
